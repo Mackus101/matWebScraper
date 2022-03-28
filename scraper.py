2042a91f21de57122f478ec43f6b08ac245e6190
Mackinley Shaw <mackshaw@iinet.net.au>
2022-03-28 12:09:48 +1100

    Now scrapes for all properties

--- scraper.py
+++ scraper.py
@@ -1,50 +1,79 @@
-from pickle import FALSE
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-options = Options()
-options.headless = FALSE
+def pick_material(page):
+    # Pick Nickel
+    metals = page.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn3')
+    metals.click()
+    page.implicitly_wait(0.5)
+    non_ferrous = page.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn13')
+    non_ferrous.click()
+    page.implicitly_wait(0.5)
+    nickel = page.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewt23')
+    nickel.click()
+    return None
 
-browser = webdriver.Chrome('driver\chromedriver.exe', options=options)
-
-browser.get('https://www.matweb.com/search/PropertySearch.aspx')
-
-metals = browser.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn3')
-metals.click()
-browser.implicitly_wait(0.5)
-non_ferrous = browser.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewn13')
-non_ferrous.click()
-browser.implicitly_wait(0.5)
-nickel = browser.find_element(By.ID, 'ctl00_ContentMain_ucMatGroupTree_LODCS1_msTreeViewt23')
-nickel.click()
-
-find = browser.find_element(By.NAME, 'ctl00$ContentMain$btnSubmit')
-find.click()
-
-current_page = 0
-page_total = int(browser.find_element(By.ID, 'ctl00_ContentMain_UcSearchResults1_lblPageTotal2').text)
-
-## dropdown = Select(browser.find_element(By.NAME, 'ctl00$ContentMain$UcSearchResults1$drpPageSelect2'))
-
-results = browser.find_element(By.ID, 'tblResults')
-
-links = []
-
-while (current_page <= 19):
-    browser.implicitly_wait(0.5)
-    dropdown = Select(browser.find_element(By.NAME, 'ctl00$ContentMain$UcSearchResults1$drpPageSelect2'))
-    results = browser.find_element(By.ID, 'tblResults')
-    links_raw = results.find_elements(By.XPATH, '//a[contains(@href, "MatGUID")]')
-    for link in links_raw:
-        links.append(link.get_attribute('href'))
-    dropdown.select_by_index(current_page)
-    current_page += 1
+def scan(page):
+    # Scan all the pages
+    current_page = 0
+    page_total = int(page.find_element(By.ID, 'ctl00_ContentMain_UcSearchResults1_lblPageTotal2').text)
     
-for link in links:
-    print(link)
+    results = page.find_element(By.ID, 'tblResults')
+    
+    links = set()
+    
+    while (current_page <= (page_total - 1)):
+        page.implicitly_wait(0.5)
+        dropdown = Select(page.find_element(By.NAME, 'ctl00$ContentMain$UcSearchResults1$drpPageSelect2'))
+        results = page.find_element(By.ID, 'tblResults')
+        links_raw = results.find_elements(By.XPATH, '//a[contains(@href, "MatGUID")]')
+        for link in links_raw:
+            links.add(link.get_attribute('href'))
+        dropdown.select_by_index(current_page)
+        current_page += 1
+        
+    return links
 
-print(len(links))
-browser.close()
+def scrape_properties(page, properties):
+
+    links = set()
+    
+    for property in properties:
+        dropdown = Select(page.find_element(By.NAME, 'ctl00$ContentMain$ucPropertyDropdown1$drpPropertyList'))
+        dropdown.select_by_visible_text(property)
+        
+        find = page.find_element(By.NAME, 'ctl00$ContentMain$btnSubmit')
+        find.click()
+        
+        links.update(scan(page))
+        page.implicitly_wait(0.5)
+        
+    return links
+
+
+if (__name__ == '__main__'):
+    properties = ['ELONGATION [PROPERTY GROUP]',
+                'Tensile Strength, Ultimate (66430 matls)',
+                'Tensile Strength, Yield (46238 matls)']
+
+    options = Options()
+    options.headless = False
+
+    browser = webdriver.Chrome('driver\chromedriver.exe', options=options)
+
+    browser.get('https://www.matweb.com/search/PropertySearch.aspx')
+
+    browser.implicitly_wait(0.5)
+    
+    pick_material(browser)
+    scan_result = scrape_properties(browser, properties)
+    
+    for link in scan_result:
+        print(link)
+
+    print(len(scan_result))
+    browser.close()
