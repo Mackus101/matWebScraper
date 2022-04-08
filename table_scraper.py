ee3839270de15fc55c06582dff89ce7a845480d5
Mackinley Shaw <mackshaw@iinet.net.au>
2022-04-08 16:35:23 +1000

    still testing

--- table_scraper.py
+++ table_scraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 import link_scraper as ls
 import time
 import random
@@ -43,10 +45,10 @@ def grab_data(link, browser):
 if __name__ == '__main__':
 
     options = Options()
-    options.headless = False
-    # options.add_argument("--user-data-dir=C:/Users/Mack/AppData/Local/Google/Chrome/User Data/Default")
+    options.headless = True
+    options.add_argument('no-sandbox')
+    browser = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=options)
 
-    browser = webdriver.Chrome('driver\chromedriver.exe', options=options)
     browser.get('https://www.matweb.com/search/PropertySearch.aspx')
     browser.implicitly_wait(0.5)
     
