9ce878ec1a12c2fbcc2bf4925496740f8b8875f4
Mackinley Shaw <mackshaw@iinet.net.au>
2022-04-08 15:32:18 +1000

    headless

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if (__name__ == '__main__'):
     #             'Tensile Strength, Yield (46238 matls)']
 
     options = Options()
-    options.headless = False
+    options.headless = True
     options.add_argument('no-sandbox')
     browser = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     # browser = webdriver.Chrome('driver\chromedriver.exe', options=options)
